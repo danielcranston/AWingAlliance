13dde8daa8d984e4fbe8be55cf1ad2c647a569be
Daniel Cranston <cranston.daniel@gmail.com>
2019-11-14 23:17:21 +0900

    add scenario parser and example scenario file

---
+++ include/parser.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class ScenarioParser
+{
+public:
+	ScenarioParser() {};
+	ScenarioParser(std::string filename);
+	void Parse();
+	void PrintRequiredModels();
+
+	std::string file_to_parse;
+
+
+	struct ActorEntry
+	{
+		std::string type;
+        glm::vec3 pos;
+        glm::vec3 dir;
+	};
+
+	struct TerrainEntry
+	{
+		TerrainEntry() : bUse(false) {}
+		bool bUse;
+		std::int32_t x;
+		std::int32_t z;
+		std::int32_t maxHeight;
+		std::int32_t blockSize;
+		std::int32_t seed;
+		std::string heightmap;
+		std::vector<std::string> textures;
+	};
+
+	struct SquadronEntry
+	{
+		std::string actor;
+		glm::vec3 pos;
+		glm::vec3 dir;
+		std::int32_t members;
+		std::string team;
+	};
+
+	std::set<std::string> required_models;
+	std::map<std::string, ActorEntry> actors;
+	std::map<std::string, SquadronEntry> squadrons;
+	
+	std::string player;
+	std::string skybox;
+	TerrainEntry terrain;
+};
