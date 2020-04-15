e2dd34333a2b978547f326b70dcb0d1c886f0d09
Daniel Cranston <cranston.daniel@gmail.com>
2020-04-15 22:14:13 +0900

    spring cleaning: terrain (finally)

--- include/terrain.h
+++ include/terrain.h
@@ -3,27 +3,19 @@
 #include <vector>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <parser.h>
 
-class Terrain
+struct Terrain
 {
-    public:
-        std::vector<float> heightmap;
-        std::vector<glm::vec3> normalmap;
-        std::vector<unsigned int> indices;
-        std::int32_t xSize, zSize, yMax, blockScale;
-        unsigned int vao, vboVertices, vboIndices;
-        unsigned int program;
-        std::vector<unsigned int> texture_ids;
-        int numTriangles;
+    explicit Terrain(const ScenarioParser::TerrainEntry* terrainentry);
 
-        explicit Terrain(const ScenarioParser::TerrainEntry &terrainentry, std::map<std::string, uint> *textures, uint program);
-        void Generate();
-        void PushToGPU();
-        void Draw(glm::mat4 camprojMat);
-        std::pair<float, glm::vec3> GetHeight(float x, float z);
-        float HeightAt(int x, int z);
+    std::pair<float, glm::vec3> GetHeight(float x, float z);
+    float HeightAt(int x, int z);
+    std::pair<std::vector<float>, std::vector<uint>> CreateBuffers() const;
 
-        bool saveBMP(const std::string& path);
+    bool saveBMP(const std::string& path);
+
+    std::vector<float> heightmap;
+    std::vector<glm::vec3> normalmap;
+    std::int32_t width, length, max_height, block_size;
 };
