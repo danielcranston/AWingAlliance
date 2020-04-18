5de3182eeeb496dd694b68bc823fb022dbabc5b9
Daniel Cranston <cranston.daniel@gmail.com>
2020-04-18 16:50:37 +0900

    move actor files to subfolder

---
+++ include/actor/actor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+// #define GLM_ENABLE_EXPERIMENTAL
+// #include <glm/gtc/quaternion.hpp>
+// #include <glm/gtx/quaternion.hpp>
+// #include <glm/gtx/string_cast.hpp>
+
+#include "model.h"
+
+namespace actor
+{
+class Actor
+{
+  public:
+    static std::unique_ptr<Actor> Create(const glm::vec3& p, const glm::vec3& d, const Model* mdl);
+
+    void SetPosition(const glm::vec3& pos);
+    void SetDirection(const glm::vec3& dir);
+    void SetPose(const glm::vec3& pos, const glm::vec3& dir);
+
+    const glm::vec3& GetPosition() const;
+    const glm::vec3& GetDirection() const;
+    const glm::mat4 GetPose() const;
+
+    const Model* GetModel() const;
+    const glm::vec3& GetColor() const;
+
+    // These can be pure virtual once I start only making unique ptrs to derived
+    virtual void Update(const float dt);
+    virtual void PrintInfo() const {};
+
+  protected:
+    explicit Actor(const glm::vec3& p, const glm::vec3& d, const Model* mdl);
+    glm::vec3 pos;
+    glm::vec3 dir;
+    const Model* model;
+    glm::vec3 color = { 0.0f, 0.0f, 0.7f };
+};
+}  // namespace actor
