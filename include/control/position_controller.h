cc30ffbb252cfa17a8826519a5203e877a516a4e
Daniel Cranston <cranston.daniel@gmail.com>
2021-05-22 13:32:43 +0900

    move hardcoded position_controller A/B/Q/R matrices to cpp file

--- include/control/position_controller.h
+++ include/control/position_controller.h
@@ -7,50 +7,14 @@
 
 namespace control
 {
-static constexpr int STATE_DIM = 9;
-static constexpr int CONTROL_DIM = 3;
-
-static const Eigen::Matrix<float, STATE_DIM, STATE_DIM> A = []() {
-    auto A = Eigen::Matrix<float, STATE_DIM, STATE_DIM>();
-    A.row(0) << 0, 0, 0, 1, 0, 0, 0, 0, 0;
-    A.row(1) << 0, 0, 0, 0, 1, 0, 0, 0, 0;
-    A.row(2) << 0, 0, 0, 0, 0, 1, 0, 0, 0;
-    A.row(3) << 0, 0, 0, 0, 0, 0, 1, 0, 0;
-    A.row(4) << 0, 0, 0, 0, 0, 0, 0, 1, 0;
-    A.row(5) << 0, 0, 0, 0, 0, 0, 0, 0, 1;
-    A.row(6) << 0, 0, 0, 0, 0, 0, 1, 0, 0;
-    A.row(7) << 0, 0, 0, 0, 0, 0, 0, 1, 0;
-    A.row(8) << 0, 0, 0, 0, 0, 0, 0, 0, 1;
-    return A;
-}();
-
-static const Eigen::Matrix<float, STATE_DIM, CONTROL_DIM> B = []() {
-    auto B = Eigen::Matrix<float, STATE_DIM, CONTROL_DIM>();
-    B.row(0) << 0, 0, 0;
-    B.row(1) << 0, 0, 0;
-    B.row(2) << 0, 0, 0;
-    B.row(3) << 0, 0, 0;
-    B.row(4) << 0, 0, 0;
-    B.row(5) << 0, 0, 0;
-    B.row(6) << 1, 0, 0;
-    B.row(7) << 0, 1, 0;
-    B.row(8) << 0, 0, 1;
-    return B;
-}();
-
-static const Eigen::Matrix<float, STATE_DIM, STATE_DIM> Q = []() {
-    auto Q = Eigen::DiagonalMatrix<float, STATE_DIM>();
-    Q.diagonal() << 1, 1, 1, 2, 2, 2, 1, 1, 1;
-    return Q;
-}();
-
-static const Eigen::Matrix<float, CONTROL_DIM, CONTROL_DIM> R =
-    Eigen::Vector3f(1, 1, 1).asDiagonal();
-
 class PositionController
 {
   public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    static constexpr int STATE_DIM = 9;
+    static constexpr int CONTROL_DIM = 3;
+
     PositionController(const Eigen::Vector3f& pos);
 
     void update(const float dt);
--- src/control/position_controller.cpp
+++ src/control/position_controller.cpp
@@ -2,6 +2,50 @@
 
 namespace control
 {
+namespace
+{
+constexpr auto STATE_DIM = PositionController::STATE_DIM;
+constexpr auto CONTROL_DIM = PositionController::CONTROL_DIM;
+
+static const Eigen::Matrix<float, STATE_DIM, STATE_DIM> A = []() {
+    auto A = Eigen::Matrix<float, STATE_DIM, STATE_DIM>();
+    A.row(0) << 0, 0, 0, 1, 0, 0, 0, 0, 0;
+    A.row(1) << 0, 0, 0, 0, 1, 0, 0, 0, 0;
+    A.row(2) << 0, 0, 0, 0, 0, 1, 0, 0, 0;
+    A.row(3) << 0, 0, 0, 0, 0, 0, 1, 0, 0;
+    A.row(4) << 0, 0, 0, 0, 0, 0, 0, 1, 0;
+    A.row(5) << 0, 0, 0, 0, 0, 0, 0, 0, 1;
+    A.row(6) << 0, 0, 0, 0, 0, 0, 1, 0, 0;
+    A.row(7) << 0, 0, 0, 0, 0, 0, 0, 1, 0;
+    A.row(8) << 0, 0, 0, 0, 0, 0, 0, 0, 1;
+    return A;
+}();
+
+static const Eigen::Matrix<float, STATE_DIM, CONTROL_DIM> B = []() {
+    auto B = Eigen::Matrix<float, STATE_DIM, CONTROL_DIM>();
+    B.row(0) << 0, 0, 0;
+    B.row(1) << 0, 0, 0;
+    B.row(2) << 0, 0, 0;
+    B.row(3) << 0, 0, 0;
+    B.row(4) << 0, 0, 0;
+    B.row(5) << 0, 0, 0;
+    B.row(6) << 1, 0, 0;
+    B.row(7) << 0, 1, 0;
+    B.row(8) << 0, 0, 1;
+    return B;
+}();
+
+static const Eigen::Matrix<float, STATE_DIM, STATE_DIM> Q = []() {
+    auto Q = Eigen::DiagonalMatrix<float, STATE_DIM>();
+    Q.diagonal() << 1000, 1000, 1000, 1000, 1000, 1000, 1, 1, 1;
+    return Q;
+}();
+
+static const Eigen::Matrix<float, CONTROL_DIM, CONTROL_DIM> R =
+    Eigen::Vector3f(0.1, 0.1, 0.1).asDiagonal();
+
+}  // namespace
+
 PositionController::PositionController(const Eigen::Vector3f& pos) : lqr_controller(A, B, Q, R)
 {
     Eigen::Matrix<float, STATE_DIM, 1> x = Eigen::Matrix<float, STATE_DIM, 1>::Zero();
