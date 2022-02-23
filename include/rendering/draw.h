52ba0e11f5e520dfd55135897ebd6694b98b0274
Daniel Cranston <cranston.daniel@gmail.com>
2022-02-23 14:15:34 +0900

    update draw.{h,cpp} to fit better with ECS setup

--- include/rendering/draw.h
+++ include/rendering/draw.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <map>
-#include <optional>
-
 #include <Eigen/Dense>
 
 #include "rendering/shader_program.h"
@@ -10,18 +7,16 @@
 
 namespace rendering
 {
-void draw(const ShaderProgram& program,
-          const Model& model,
-          const Eigen::Isometry3f& pose,
-          const Eigen::Vector3f& color,
-          const std::map<std::string, rendering::Texture>& textures_map,
-          const int mode);
+void draw_textured(const ShaderProgram& program,
+                   const Mesh& mesh,
+                   const Eigen::Isometry3f& pose,
+                   const Texture& texture,
+                   const int mode);
 
-void draw(const ShaderProgram& program,
-          const Model& model,
-          const Eigen::Isometry3f& pose,
-          const Eigen::Vector3f& color,
-          const std::optional<std::reference_wrapper<const Texture>> texture,
-          const int mode);
+void draw_colored(const ShaderProgram& program,
+                  const Mesh& mesh,
+                  const Eigen::Isometry3f& pose,
+                  const Eigen::Vector3f& color,
+                  const int mode);
 
 }  // namespace rendering
--- src/rendering/draw.cpp
+++ src/rendering/draw.cpp
@@ -7,98 +7,53 @@
 namespace rendering
 {
 void draw(const ShaderProgram& program,
-          const Model& model,
+          const Mesh& mesh,
           const Eigen::Isometry3f& pose,
-          const Eigen::Vector3f& color,
-          const std::map<std::string, rendering::Texture>& textures_map,
           const int mode)
 {
-    program.use();
     program.setUniformMatrix4fv("model_pose", pose.matrix());
-    for (const auto& mesh : model.get_meshes())
-    {
-        if (mesh.has_texture())
-        {
-            program.setUniform1i("use_color", 0);
 
-            if (textures_map.find(mesh.get_texture_name()) == textures_map.end())
-            {
-                throw std::runtime_error("draw: texture not found in textures map: " +
-                                         mesh.get_texture_name());
-            }
+    if (mode != GL_TRIANGLES && mode != GL_LINES)
+        throw std::runtime_error("Invalid draw mode " + std::to_string(mode));
 
-            const Texture& tex = textures_map.at(mesh.get_texture_name());
-            if (tex.type == Texture::Type::TEXTURE)
-            {
-                glBindTexture(GL_TEXTURE_2D, tex.texture_id);
-            }
-            else if (tex.type == Texture::Type::CUBEMAP)
-            {
-                glBindTexture(GL_TEXTURE_CUBE_MAP, tex.texture_id);
-            }
-            else
-            {
-                throw std::runtime_error("Unexpected texture type: " +
-                                         std::to_string(static_cast<int>(tex.type)));
-            }
-        }
-        else
-        {
-            program.setUniform1i("use_color", 1);
-            program.setUniform3fv("uniform_color", color);
-        }
-
-        if (mode != GL_TRIANGLES && mode != GL_LINES)
-            throw std::runtime_error("Invalid draw mode " + std::to_string(mode));
-
-        glBindVertexArray(mesh.get_vao());
-        glDrawElements(mode, mesh.get_num_indices(), GL_UNSIGNED_INT, (const void*)0);
-    }
+    glBindVertexArray(mesh.get_vao());
+    glDrawElements(mode, mesh.get_num_indices(), GL_UNSIGNED_INT, (const void*)0);
 }
 
-void draw(const ShaderProgram& program,
-          const Model& model,
-          const Eigen::Isometry3f& pose,
-          const Eigen::Vector3f& color,
-          const std::optional<std::reference_wrapper<const Texture>> texture,
-          const int mode)
+void draw_textured(const ShaderProgram& program,
+                   const Mesh& mesh,
+                   const Eigen::Isometry3f& pose,
+                   const Texture& texture,
+                   const int mode)
 {
-    program.use();
-    program.setUniformMatrix4fv("model_pose", pose.matrix());
-    for (const auto& mesh : model.get_meshes())
+    program.setUniform1i("use_color", 0);
+    if (texture.type == Texture::Type::TEXTURE)
     {
-        if (texture.has_value())
-        {
-            const Texture& tex = texture.value().get();
-            program.setUniform1i("use_color", 0);
-            if (tex.type == Texture::Type::TEXTURE)
-            {
-                glBindTexture(GL_TEXTURE_2D, tex.texture_id);
-            }
-            else if (tex.type == Texture::Type::CUBEMAP)
-            {
-                glBindTexture(GL_TEXTURE_CUBE_MAP, tex.texture_id);
-            }
-            else
-            {
-                throw std::runtime_error("Unexpected texture type: " +
-                                         std::to_string(static_cast<int>(tex.type)));
-            }
-        }
-        else
-        {
-            std::cout << "is not texture" << std::endl;
-            program.setUniform1i("use_color", 1);
-            glBindTexture(GL_TEXTURE_2D, 0);
-            program.setUniform3fv("uniform_color", color);
-        }
-
-        if (mode != GL_TRIANGLES && mode != GL_LINES)
-            throw std::runtime_error("Invalid draw mode " + std::to_string(mode));
-
-        glBindVertexArray(mesh.get_vao());
-        glDrawElements(mode, mesh.get_num_indices(), GL_UNSIGNED_INT, (const void*)0);
+        glBindTexture(GL_TEXTURE_2D, texture.texture_id);
     }
+    else if (texture.type == Texture::Type::CUBEMAP)
+    {
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texture.texture_id);
+    }
+    else
+    {
+        throw std::runtime_error("Unexpected texture type: " +
+                                 std::to_string(static_cast<int>(texture.type)));
+    }
+
+    draw(program, mesh, pose, mode);
+}
+
+void draw_colored(const ShaderProgram& program,
+                  const Mesh& mesh,
+                  const Eigen::Isometry3f& pose,
+                  const Eigen::Vector3f& color,
+                  const int mode)
+{
+    program.setUniform1i("use_color", 1);
+    program.setUniform3fv("uniform_color", color);
+
+    draw(program, mesh, pose, mode);
 }
 
 }  // namespace rendering
